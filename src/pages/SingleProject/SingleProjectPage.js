70054d8f6e2935aa90e88b8d43454c4c99ae7d65
RParmasar <rajeev@parmasar.com>
2023-01-08 10:13:34 -0500

    Fixing build errors

--- src/pages/SingleProject/SingleProjectPage.js
+++ src/pages/SingleProject/SingleProjectPage.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 
-import Header from '../../common/Header/Header';
-import Section from '../../common/Section/Section';
+// import Header from '../../common/Header/Header';
+// import Section from '../../common/Section/Section';
 import ProjectLanding from './ProjectLanding/ProjectLanding';
 
 import './SingleProjectPage.css';
